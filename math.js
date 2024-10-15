bd54bfaa90a09716179d310e6191ba534e0d4003
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-15 21:34:09 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+// *********************************
+// math needed fo programming
+// *********************************
+// 1. number : -ve , +ve 0 [50 ,-30 ,0]
+// 2.integer : 2 ,378[ odd ,even]:2,4,6 ,1,3.5
+// 3. fraction : 3.4 ,4.5 
+// operation : - ,+.* ,/
+// 5. more operation : %,[]
+// 6. percentage :5%, 50 er 3% koto 
+// 7.sum: 12,36,48
+// 8. avg: 
+// 9.max/ min 
+// 10. series:
+// 11. area: triangle 
+// 12.
---
+++ variable.js
@@ -0,0 +1,13 @@
+// variable > vary +able 
+// 5 things to declare a variable 
+var weight = 38 ;
+var price = 38;
+var year = 1965;
+var money =28894;
+var age ;
+console.log(money);
+console.log(price);
+
+var age =12 ;
+age =45;
+console.log(age)

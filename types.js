100bb2d6b8bfa8233f40f7c0b86ba9d3bce26a10
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-16 12:32:26 +0600

    added types file

---
+++ types.js
@@ -0,0 +1,26 @@
+// name ta double cottation er modde rakhte hobe .
+
+// string- name
+var name= "raisha";
+console.log(name)
+
+var address ="university of chittagong"
+console.log(address);
+
+var country='bangladesh'
+console.log(country);
+
+
+// boolean type variable -eaxctly true/false
+var isrich=true;
+var passed =false;
+
+console.log(isrich);
+
+// number
+var money=20;
+
+// type of variable
+console.log(typeof country);
+console.log(typeof isrich);
+console.log(typeof money);

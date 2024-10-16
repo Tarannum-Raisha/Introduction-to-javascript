5ec3243acd9d6bf93e8db3a3a1138dd78e6f6671
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-16 13:39:53 +0600

    added number and mixed and shorthand file

---
+++ mixed.js
@@ -0,0 +1,9 @@
+var first = 'mobarak';
+ var second = 'tobarak';
+  var fullname = first + second;
+//    console.log(fullname);
+
+// console.log(40+ '2');
+// console.log(40+ 2);
+console.log(40-'2'); //biyog mane na
+console.log('50'-10);
---
+++ shorthand.js
@@ -0,0 +1,15 @@
+ var price = 34;
+//   price = price + 10;
+price += 10; //price = price + 10
+
+//   console.log(price);
+
+  var  fridge = 35000;
+  fridge -= 1000;  //fridge =fridge - 1000;
+//   console.log(fridge)
+
+// multiply
+var orrangePrice = 30;
+// orrangePrice = orrangePrice * 4;
+orrangePrice *=  4;
+console.log(orrangePrice);

d64eea503b1bd3c6c25776ff0e63f6867e1923dc
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-16 13:15:06 +0600

    added number file

---
+++ number.js
@@ -0,0 +1,18 @@
+var orrangePrice =20 ;
+var  chocolate = 0.5;
+// var applePrice = parseInt('20');
+
+// var applePrice = parseInt('22.5');
+var applePrice = parseFloat('22.5');
+
+//  console.log(orrangePrice);
+//  console.log(applePrice);
+// console.log(orrangePrice + applePrice);
+ var first = 0.6;
+ var second = 0.2;
+  var total = first + second ;
+//   tofixed e strimg hoye jay 
+   console.log(total.toFixed(3));
+//  const num = 20 /0;
+ const num = -20 /0;
+ console.log(num);

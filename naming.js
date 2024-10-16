86eeeeda9f875089c2de02159cf905a832855a93
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-16 12:48:15 +0600

    added naming file

---
+++ naming.js
@@ -0,0 +1,31 @@
+// 1.no key word in variable name 
+ var false=24;
+ var var=23;
+
+//  2. no space or gap in variable name 
+var is happy =false;
+var ishappy= true;
+
+// 3. no quote 
+var "address" = "Dhaka";
+
+// 4. cam not start a number but number other then first letter is allow 
+
+ var money = 45;
+ var money2 =36;
+
+   var 23money=23;
+
+
+// 5. name is case sensitive
+ address
+Address
+ADDRESS 
+addRAss
+
+// 6. now to type a long variable name
+var my current home address = "University of chittagong ";
+var myCurrentHomeAddress = "University of chittagong "; //Camel case 
+var MyCurrentHomeAddress = "University of chittagong ";  //pascal case
+ var my_current_home_address = "University of chittagong ";   //snake case
+var my current home address = "University of chittagong ";
